6f53e8242f013402080197ab7ddf7b879cb82135
paymon <paymon@safexapp.com>
2026-06-08 14:18:20 +0800

    4

--- contracts/CCIPSimulator.sol
+++ contracts/CCIPSimulator.sol
@@ -1,4 +1,5 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+// 用来提供一个本地模拟器，帮助开发者在本地环境中测试 CCIP 相关的功能，而无需连接到实际的链上网络。 router link Address
 import "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
